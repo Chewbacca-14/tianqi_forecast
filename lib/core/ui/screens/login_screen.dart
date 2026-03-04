833ae25beed1915335b9bc82d9a00dd70bbb5d23
Banduga) <www.swarm1996.by@gmail.com>
2026-03-04 14:49:46 +0300

    login and reg fix

--- lib/core/ui/screens/login_screen.dart
+++ lib/core/ui/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:tianqi_forecast/core/ui/screens/registration_screen.dart';
 import 'package:tianqi_forecast/core/ui/widgets/custom_button.dart';
 import 'package:tianqi_forecast/core/ui/widgets/custom_text_field.dart';
 
@@ -11,8 +10,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isLogin = true;
   final emailAddressController = TextEditingController();
   final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  void changeScreen() {
+    setState(() {
+      isLogin == true ? isLogin = false : isLogin = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Padding(
-              padding: const EdgeInsets.only(top: 40.0, bottom: 24),
+              padding: const EdgeInsets.only(top: 40.0),
               child: Container(
                 width: 76,
                 height: 64,
@@ -39,29 +46,26 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 15.0, bottom: 9),
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  color: Color(0xFFF1F5F9),
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                ),
+            SizedBox(height: 39),
+            Text(
+              isLogin == true ? "Login" : "Join Tianqi Forecast",
+              style: TextStyle(
+                color: Color(0xFFF1F5F9),
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 24.0),
-              child: Text(
-                "Get hyper-local forecasts and severe\nweather alerts.",
-                style: TextStyle(
-                  color: Color(0xFF94A3B8),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
-                ),
-                textAlign: TextAlign.center,
+            SizedBox(height: 9),
+            Text(
+              "Get hyper-local forecasts and severe\nweather alerts.",
+              style: TextStyle(
+                color: Color(0xFF94A3B8),
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
               ),
+              textAlign: TextAlign.center,
             ),
+            SizedBox(height: 24),
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -74,13 +78,12 @@ class _LoginScreenState extends State<LoginScreen> {
                 textAlign: TextAlign.center,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 16),
-              child: CustomTextField(
-                controller: emailAddressController,
-                hintText: "name@example.com",
-              ),
+            SizedBox(height: 8),
+            CustomTextField(
+              controller: emailAddressController,
+              hintText: "name@example.com",
             ),
+            SizedBox(height: 16),
             Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -93,57 +96,73 @@ class _LoginScreenState extends State<LoginScreen> {
                 textAlign: TextAlign.start,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0, bottom: 16),
-              child: CustomTextField(
-                controller: passwordController,
-                hintText: "Password",
-                isPassword: true,
-              ),
+            SizedBox(height: 8),
+            CustomTextField(
+              controller: passwordController,
+              hintText: isLogin == true ? "Password" : "Create a password",
+              isPassword: true,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 24.0, bottom: 16),
-              child: CustomButton(
-                fontSize: 16,
-                fontWeight: FontWeight.w700,
-                color: Color(0xFFFFFFFF),
-                label: "Login",
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 31),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don’t have an account?",
+            isLogin == true
+                ? SizedBox.shrink()
+                : Column(
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Confirm password",
                     style: TextStyle(
-                      color: Color(0xFF94A3B8),
-                      fontWeight: FontWeight.w400,
+                      color: Color(0xFFCBD5E1),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                SizedBox(height: 8),
+                CustomTextField(
+                  controller: confirmPasswordController,
+                  hintText: "Repeat your password",
+                  isPassword: true,
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            CustomButton(
+              fontSize: 16,
+              fontWeight: FontWeight.w700,
+              color: Color(0xFFFFFFFF),
+              label: isLogin ==true? "Login":"Create account",
+            ),
+
+            SizedBox(height: 47),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  isLogin ==true?
+                  "Don’t have an account?":"Already have an account? ",
+                  style: TextStyle(
+                    color: Color(0xFF94A3B8),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    changeScreen();
+                  },
+
+                  child: Text(
+                    isLogin==true?
+                    "Sign up":"Log in",
+                    style: TextStyle(
+                      color: Color(0xFF4B91E2),
+                      fontWeight: FontWeight.w700,
                       fontSize: 14,
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => RegistrationScreen(),
-                        ),
-                      );
-                    },
-
-                    child: Text(
-                      "Sign up",
-                      style: TextStyle(
-                        color: Color(0xFF4B91E2),
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ],
         ),
